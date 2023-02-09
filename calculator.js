124ff271742e8525acdc81a1adee27119f7dda57
Arshdeep Arora <arsharora017@gmail.com>
2023-02-09 13:31:25 -0500

    add decimal function to disable decimal button if decimal already exists in currentNum.

--- calculator.js
+++ calculator.js
@@ -8,21 +8,25 @@ const currentDisplayNumber = document.querySelector(".current-number");
 const previousDisplayNumber = document.querySelector(".previous-number");
 
 const equal = document.querySelector(".equal");
-equal.addEventListener("click", calculate);
+equal.addEventListener("click", () => {
+  if (currentNum != "" && previousNum != "") {
+    calculate();
+  }
+});
 
 const clear = document.querySelector(".clear");
+clear.addEventListener("click", clearDisplay);
+
+//listening decimal button click and firing the fn
+const decimal = document.querySelector("#decimal");
+decimal.addEventListener("click", () => {
+  addDecimal();
+});
 
 // window.addEventListener("keydown", function (e) {
 //   const key = document.querySelector(`button[data-key="${e.keyCode}"]`);
 //   key.click();
 // });
-
-// function updateDisplay() {
-//   const display = document.querySelector(".display");
-//   display.innerText = "0";
-// }
-
-//updateDisplay();
 
 //added eventLintener to each operand button click
 numberButtons.forEach((btn) => {
@@ -33,12 +37,18 @@ numberButtons.forEach((btn) => {
 
 //displaying numbers on the display after every button click
 function handleNumber(number) {
+  if (previousNum !== "" && currentNum !== "" && operator === "") {
+    previousNum = "";
+    currentDisplayNumber.textContent = currentNum;
+  }
   if (currentNum.length < 8) {
     currentNum += number;
     //currentNum and number are string
     //we are concatintating string
     currentDisplayNumber.textContent = currentNum;
   }
+  // if (currentNum.includes(".")) {
+  // }
 }
 
 operatorButtons.forEach((btn) => {
@@ -48,11 +58,24 @@ operatorButtons.forEach((btn) => {
 });
 
 function handleOperator(op) {
-  operator = op;
-  previousNum = currentNum;
-  previousDisplayNumber.textContent = previousNum + op;
+  if (previousNum === "") {
+    previousNum = currentNum;
+    operatorCheck(op);
+  } else if (currentNum === "") {
+    operatorCheck(op);
+  } else {
+    calculate();
+    operator = op;
+    currentDisplayNumber.textContent = "0";
+    previousDisplayNumber.textContent = previousNum + " " + operator;
+  }
+}
+
+function operatorCheck(val) {
+  operator = val;
+  previousDisplayNumber.textContent = previousNum + " " + operator;
+  currentDisplayNumber.textContent = "0";
   currentNum = "";
-  currentDisplayNumber.textContent = "";
 }
 
 function calculate() {
@@ -72,13 +95,42 @@ function calculate() {
     previousNum = previousNum / currentNum;
   } else if (operator === "x") {
     previousNum = previousNum * currentNum;
+  } else if (operator === "%") {
+    previousNum = 100 / currentNum;
   }
+
   previousNum = previousNum.toString();
   displayResults();
   operator = "";
 }
 
 function displayResults() {
+  //currentDisplayNumber.textContent = previousNum;
+  if (previousNum.length < 8) {
+    currentDisplayNumber.textContent = previousNum;
+  } else {
+    currentDisplayNumber.textContent = previousNum.slice(0, 8);
+  }
   previousDisplayNumber.textContent = ""; //clearing prev number
-  currentDisplayNumber.textContent = previousNum;
+  operator = "";
+  currentNum = "";
+}
+
+function roundNumber(num) {
+  return Math.round(num * 100000) / 10000;
+}
+
+function clearDisplay() {
+  previousNum = "";
+  currentNum = "";
+  operator = "";
+  previousDisplayNumber.textContent = "";
+  currentDisplayNumber.textContent = "0";
+}
+
+function addDecimal() {
+  if (!currentNum.includes(".")) {
+    currentNum += ".";
+    currentDisplayNumber.textContent = currentNum;
+  }
 }
